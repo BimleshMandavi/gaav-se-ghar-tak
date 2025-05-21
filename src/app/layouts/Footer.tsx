c3078d839b462a58a25d9e1ddff0cfaddf568979
Bimlesh Mandavi <bimleshmandavi0@gmail.com>
2025-05-21 22:28:53 +0530

    add product_categories

--- src/app/layouts/Footer.tsx
+++ src/app/layouts/Footer.tsx
@@ -6,14 +6,14 @@ import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
     <div>
-      <div className="bg-[#3e1a00] flex justify-around items-center flex-col md:flex-row p-10">
+      <div className="bg-[#3e1a00] flex justify-around items-center flex-col md:flex-row sm:p-10 p-4">
         <div className="flex justify-evenly gap-5 flex-col">
-          <h1 className="md:text-5xl text-[#e8e0c3]">Contact</h1>
-          <p className="md:text-2xl text-[#e8c8ae]">info@gaavseghartak.com</p>
-          <p className="text-2xl text-[#e8c8ae]">+91 9301269119</p>
+          <h1 className="text-2xl md:text-5xl text-[#e8e0c3]">Contact</h1>
+          <p className="text-xl md:text-2xl text-[#e8c8ae]">info@gaavseghartak.com</p>
+          <p className="text-xl md:text-2xl text-[#e8c8ae]">+91 9301269119</p>
         </div>
-        <div className="flex justify-evenly gap-5 flex-col">
-          <h1 className="md:text-4xl text-[#e8e0c3]">Social</h1>
+        <div className="flex justify-evenly gap-5 flex-col pt-5 ">
+          <h1 className="text-2xl md:text-4xl text-[#e8e0c3]">Social</h1>
           <div className="flex gap-5 text-4xl">
             <a
               href="https://x.com/Gaavseghartak"
@@ -33,9 +33,9 @@ const Footer = () => {
             <FaInstagram />
           </div>
         </div>
-        <div className="flex justify-evenly gap-5 flex-col">
-          <h1 className="md:text-4xl text-[#e8e0c3]">Susser vre</h1>
-          <h1 className="md:text-4xl text-[#e8e0c3]">to our newsletter</h1>
+        <div className="flex justify-evenly gap-5 flex-col pt-5">
+          <h1 className="text-2xl md:text-4xl text-[#e8e0c3]">Subscribe</h1>
+          <h1 className="text-2xl md:text-4xl text-[#e8e0c3]">to our newsletter</h1>
           <button className="bg-[#d4ac7b]  min-w-[270px] text-black font-bold text-2xl rounded-xl cursor-pointer p-4">
             Subscribe
           </button>
--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,16 +1,41 @@
 import React from 'react'
 import Image from "next/image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
+
+  const products = [
+    {
+      title: "Mangoes",
+      img: "/pic-02.png",
+    },
+    {
+      title: "Seasonal Fruits",
+      img: "/pic-03.png",
+    },
+    {
+      title: "Pickles",
+      img: "/pic-04.png",
+    },
+    {
+      title: "Snacks",
+      img: "/pic-05.png",
+    },
+  ];
+
+
   return (
     <div>
       <div className="bg-[#bc8648] h-full w-full sm:flex justify-between items-center">
         <div className="text-[#311d00] sm:flex sm:justify-center items-center flex-col sm:w-[50%] text-start pl-10">
           <div className="p-8">
-            <h2 className="text-2xl sm:text-6xl lg:text-8xl font-bold">
+            <h2 className="text-2xl sm:text-6xl lg:text-8xl font-bold font-ancizarSerif">
               Fresh Fruits from Indian Villages
             </h2>
-            <p className="text-xl sm:text-5xl pt-10">Delivered to Your Doorstep</p>
+            <p className="text-xl sm:text-5xl pt-10">
+              Delivered to Your Doorstep
+            </p>
           </div>
         </div>
         <div className="sm:w-[50%]">
@@ -23,9 +48,31 @@ const Home = () => {
           />
         </div>
       </div>
-      <div className="bg-[#ebd7bc] h-full w-full">
-        <div className="text-[#351d0a] p-10 flex justify-center items-center">
-          <h1 className='text-2xl sm:text-6xl font-bold'>Carduct Categories</h1>
+      <div className="bg-[#ebd7bc] h-full w-full pt-10">
+        <div className='pt-10 pb-20'>
+          <h1 className="text-2xl sm:text-6xl font-bold text-[#351d0a] text-center pt-10 ">
+            Product Categories
+          </h1>
+          <div className="text-[#351d0a] p-5 flex  justify-center gap-4 items-center">
+            {[...products].map((product, index) => (
+              <div key={index}>
+                <div>
+                  <Image
+                    className="rounded-xl lg:size-80 mt-4 mb-4"
+                    src={product.img}
+                    alt={product.title}
+                    height={300}
+                    width={300}
+                  />
+                </div>
+                <div className='pt-8'>
+                  <p className="text-xl md:text-5xl  font-bold text-center ">
+                    {product.title}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
